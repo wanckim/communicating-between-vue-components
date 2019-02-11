17396c00942d02f4df83f77280418d5ec0483158
Wan Cheul Kim <wckim88@gmail.com>
2019-02-11 14:59:44 -0800

    using custom event for child to parent communication

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User name: {{ switchName() }}</p>
+        <button @click="resetName">Reset name</button>
     </div>
 </template>
 
@@ -10,13 +11,16 @@
     export default {
         props: {
             myName: {
-              type: String,
-              required: true
+              type: String
             }
         },
         methods: {
             switchName() {
                 return this.myName.split('').reverse().join('');
+            },
+            resetName() {
+              this.myName = 'Wan';
+              this.$emit('nameWasReset', this.myName);
             }
         }
     }
